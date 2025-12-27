a0bfbc9d3c69a6a7c024888818600fd5bee9ab90
riii111 <ryo.ichinose@nodex.inc>
2025-12-27 16:57:45 +0900

    feat: add async event loop with tokio

--- src/ui/event/handler.rs
+++ src/ui/event/handler.rs
@@ -1,1 +1,26 @@
-// Event handler stub
+use crossterm::event::KeyCode;
+
+use crate::app::action::Action;
+use crate::app::state::AppState;
+
+use super::Event;
+
+pub fn handle_event(event: Event, _state: &AppState) -> Action {
+    match event {
+        Event::Quit => Action::Quit,
+        Event::Render => Action::Render,
+        Event::Resize(w, h) => Action::Resize(w, h),
+        Event::Key(key) => match key.code {
+            KeyCode::Char('q') => Action::Quit,
+            KeyCode::Char('1') => Action::SwitchToBrowse,
+            KeyCode::Char('2') => Action::SwitchToER,
+            KeyCode::Char('f') => Action::ToggleFocus,
+            KeyCode::Up | KeyCode::Char('k') => Action::Up,
+            KeyCode::Down | KeyCode::Char('j') => Action::Down,
+            KeyCode::Left | KeyCode::Char('h') => Action::Left,
+            KeyCode::Right | KeyCode::Char('l') => Action::Right,
+            _ => Action::None,
+        },
+        _ => Action::None,
+    }
+}
--- src/ui/event/mod.rs
+++ src/ui/event/mod.rs
@@ -1,1 +1,14 @@
 pub mod handler;
+
+use crossterm::event::{KeyEvent, MouseEvent};
+
+#[derive(Clone, Debug)]
+pub enum Event {
+    Init,
+    Tick,
+    Render,
+    Key(KeyEvent),
+    Mouse(MouseEvent),
+    Resize(u16, u16),
+    Quit,
+}
